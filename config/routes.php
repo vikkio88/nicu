23fd9c5a9b1455705d7a3a042ce667635b4fbc3a
Vincenzo Ciaccio <vincenzo@rushpartners.co.uk>
2019-05-07 22:51:45 +0100

    project init

---
+++ config/routes.php
@@ -0,0 +1,13 @@
+<?php
+
+use Nicu\Actions\Misc\Ping;
+
+return [
+    'routes' => [
+        [
+            'route' => '/ping',
+            'method' => 'get',
+            'action' => Ping::class
+        ]
+    ]
+];
